0206edf9ec1f98b7e97cbccf0ac0b79591fcbdd1
Varazdat Melkonyan <58566427+varo-melkonyan@users.noreply.github.com>
2023-01-09 15:39:27 +0400

    as

--- cypress/e2e/Functional operations/I or O channels/failedAddEditChannels.cy.js
+++ cypress/e2e/Functional operations/I or O channels/failedAddEditChannels.cy.js
@@ -68,7 +68,6 @@ describe('Custom Malfunction', () => {
 
             //add new Buzzer channel digital
             await cy.get('.sys-accordion__header').eq(2).contains(' + Add New ').click();
-            await cy.get('.mat-checkbox-input').check({force: true});
             await cy.get('.mat-select').eq(0).click().get('.mat-option').contains("Buzzer").click();
             await cy.get('.mat-select').eq(1).click().get('.mat-option').contains("Module 8DI/8DO").click();
             await cy.get('.mat-select').eq(2).click().get('.mat-option').contains("DI 04").click();
@@ -77,7 +76,7 @@ describe('Custom Malfunction', () => {
             await cy.get('.mat-select').eq(3).click().get('.mat-option').contains("False (opened)").click();
             await cy.get('input[formcontrolname="thresholdDuration"]').clear().type(1);
             await cy.get('input[formcontrolname="malfunctionLimit"]').clear().type(3);
-            await cy.get('.mat-input-element').eq(4).clear().type(1);
+            await cy.get('.mat-input-element').eq(3).clear().type(1);
             await cy.get('.mat-flat-button').eq(1).click();
 
             await cy.wait(2000);
@@ -127,29 +126,31 @@ describe('Custom Malfunction', () => {
             //check VFD speed channel
             await cy.get('.sys-accordion__grid-item').eq(22).should('be.text', 'VFD Speed');
             await cy.get('.sys-accordion__grid-item').eq(22).contains('VFD Speed').click({force: true});
-            await cy.get('input[formcontrolname="name"]').should('have.value', "VFD Speed");
-            await cy.get('.mat-select-value-text > span').eq(0).should('contain.text', "Module SYNC 1");
-            await cy.get('.mat-select-value-text > span').eq(1).should('contain.text', "AO 02");
-            await cy.get('.mat-select-value-text > span').eq(2).should('contain.text', "0-20mA");
-            await cy.get('.mat-select-value-text > span').eq(3).should('contain.text', "Hz");
+            await cy.get('.mat-select-value-text > span').eq(0).should('contain.text', "VFD Speed");
+            await cy.get('.mat-select-value-text > span').eq(1).should('contain.text', "Module SYNC 1");
+            await cy.get('.mat-select-value-text > span').eq(2).should('contain.text', "AO 02");
+            await cy.get('.mat-select-value-text > span').eq(3).should('contain.text', "0-20mA");
+            await cy.get('.mat-select-value-text > span').eq(4).should('contain.text', "Hz");
             await cy.get('input[formcontrolname="y1"]').should('have.value', 0);
             await cy.get('input[formcontrolname="y2"]').should('have.value', 60);
             await cy.get('.mat-flat-button').eq(0).click();
 
             //check Buzzer channel
-            await cy.get('.sys-accordion__grid-item').eq(25).should('be.text', 'Buzzer');
-            await cy.get('.sys-accordion__grid-item').eq(25).contains('Buzzer').click({force: true});
+            await cy.get('.sys-accordion__grid-item').eq(26).should('be.text', 'Buzzer');
+            await cy.get('.sys-accordion__grid-item').eq(26).contains('Buzzer').click({force: true});
             await cy.get('.mat-select-value-text > span').eq(0).should('contain.text', "Buzzer");
             await cy.get('.mat-select-value-text > span').eq(1).should('contain.text', "Module 8DI/8DO");
             await cy.get('.mat-select-value-text > span').eq(2).should('contain.text', "DI 04");
             await cy.get('.mat-select-value-text > span').eq(3).should('contain.text', "False (opened)");
             await cy.get('input[formcontrolname="thresholdDuration"]').should('have.value', 1);
             await cy.get('input[formcontrolname="malfunctionLimit"]').should('have.value', 3);
+            await cy.get('.mat-input-element').eq(3).should('have.value',"01");
+
             await cy.get('.mat-flat-button').eq(0).click();
 
             //check custom Digital
-            await cy.get('.sys-accordion__grid-item').eq(28).should('be.text', 'Custom Digital');
-            await cy.get('.sys-accordion__grid-item').eq(28).contains('Custom Digital').click({force: true});
+            await cy.get('.sys-accordion__grid-item').eq(29).should('be.text', 'Custom Digital');
+            await cy.get('.sys-accordion__grid-item').eq(29).contains('Custom Digital').click({force: true});
             await cy.get('input[formcontrolname="name"]').should('have.value', "Custom Digital");
             await cy.get('.mat-select-value-text > span').eq(0).should('contain.text', "Module 8DI/8DO");
             await cy.get('.mat-select-value-text > span').eq(1).should('contain.text', "DO 04");
